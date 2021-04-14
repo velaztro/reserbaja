875d2dadcfbe1fb72a0729e54c19b737a828b6f3
velaztro <68763480+velaztro@users.noreply.github.com>
2021-04-13 21:57:04 -0700

    fonts

--- components/layout.js
+++ components/layout.js
@@ -36,7 +36,8 @@ const Layout = ({ children }) => {
     return (
         <div className={styles.container} id={ opac ? styles.opacity : null}>
             <Head>
-                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+                <link rel="dns-prefetch" href="https://fonts.gstatic.com" />
                 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet" />
                 <link rel="icon" href="/favicon.ico" />
                 <meta
