6640cd7a3300e37741fc1f1d910ecf1b9ed8dba4
velaztro <68763480+velaztro@users.noreply.github.com>
2021-04-17 03:05:05 -0700

    fix

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Layout from '../components/layout'
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Head from "next/head";
 
 
 export default function Home() {
@@ -8,12 +9,20 @@ export default function Home() {
 
 		<Layout home>
 
+			<Head>
+				<link
+					rel="preload"
+					href="/Image/index/land.jpg"
+					as="image"
+				/>
+			</Head>
+
 			<section id={styles.land}>
 				<div className={styles.image}>
-				<Image width={450} height={300} priority="true" loading="eager" alt="seguridad automotriz carro auto alarmas cerrajeria auto estereo auto carro car alarms car stereo car keys duplicate" src="/Images/index/land.jpg" layout="responsive" />
+					<Image width={450} height={300} priority="true" loading="eager" alt="seguridad automotriz carro auto alarmas cerrajeria auto estereo auto carro car alarms car stereo car keys duplicate" src="/Images/index/land.jpg" layout="responsive" />
 				</div>
 				<div className={styles.flexTitle}>
-					<h2 className={styles.landTitle}>En RESERBAJA tu carro esta seguro.</h2>
+					<h2 className={styles.landTitle}>En <b style={{color: "#0e86ff"}}>RESERBAJA</b> tu carro esta seguro.</h2>
 				</div>
 			</section>
 
