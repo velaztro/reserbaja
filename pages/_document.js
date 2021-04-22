66434ea5cc6035eee8aa003411aaf3c15b445c9c
velaztro <68763480+velaztro@users.noreply.github.com>
2021-04-21 21:04:40 -0700

    favicon fix

--- pages/_document.js
+++ pages/_document.js
@@ -25,7 +25,7 @@ class MyDocument extends Document {
                     <link rel="mask-icon" href="/safari-pinned-tab.svg?v=Gv6nAlj7bz" color="#5bbad5" />
                     <link rel="shortcut icon" href="/favicon.ico?v=Gv6nAlj7bz" />
                     <meta name="msapplication-TileColor" content="#b91d47" />
-                    <meta name="theme-color" content="#ffffff" />
+                    <meta name="theme-color" />
                     {/* Global site tag (gtag.js) - Google Analytics */}
                     <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}></script>
                     <script
