5ff347e997c8d0cd6b6f5dd53ec48cff9dca3de2
velaztro <68763480+velaztro@users.noreply.github.com>
2021-05-12 21:28:15 -0700

    pwa

--- next.config.js
+++ next.config.js
@@ -1,17 +1,28 @@
 
-
+/*
 const withPWA = require('next-pwa')
 
 module.exports = withPWA({
     future: {
         webpack5: true,
-    }
-    /*pwa: {
+    },
+    pwa: {
         dest: 'public',
         disable: process.env.NODE_ENV === 'development',
         register: true,
         skipWaiting: true,
         //        scope: '/app',
         //        sw: 'service-worker.js',
-    }*/
+    }
 })
+*/
+
+module.exports = {
+    future: {
+        webpack5: true,
+    },
+    webpack: function (config, options) {
+        config.experiments = {};
+        return config;
+    },
+};
