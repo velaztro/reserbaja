2961412fe3ef2a27f87f3d34be2ea5e1ab7a5f18
velaztro <68763480+velaztro@users.noreply.github.com>
2021-06-17 02:14:37 -0700

    gtag

--- pages/_document.js
+++ pages/_document.js
@@ -9,14 +9,7 @@ class MyDocument extends Document {
     render() {
         return (
             <Html lang="es-MX">
-                <Head>
-                    <link rel="apple-touch-icon" type='image/png' sizes="180x180" href="/favicons/apple-touch-icon.png?v=Gv6nAlj7bz" />
-                    <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png?v=Gv6nAlj7bz" />
-                    <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png?v=Gv6nAlj7bz" />
-                    <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg?v=Gv6nAlj7bz" color="#5bbad5" />
-                    <link rel="shortcut icon" href="/favicons/favicon.ico?v=Gv6nAlj7bz" />
-                    <meta name="theme-color" content="#f4f4f4" />
-                </Head>
+                <Head />
                 <body>
                     <Main />
                     <NextScript />
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,34 @@
 import Layout from '../components/Layout/layout'
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import Script from 'next/script'
+import Script from "next/script";
+import Head from 'next/head';
 
 export default function Home() {
 	return (
 
 		<Layout home>
+
+			<Head>
+				<link rel="apple-touch-icon" type='image/png' sizes="180x180" href="/favicons/apple-touch-icon.png?v=Gv6nAlj7bz" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png?v=Gv6nAlj7bz" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png?v=Gv6nAlj7bz" />
+				<link rel="mask-icon" href="/favicons/safari-pinned-tab.svg?v=Gv6nAlj7bz" color="#5bbad5" />
+				<link rel="shortcut icon" href="/favicons/favicon.ico?v=Gv6nAlj7bz" />
+				<meta name="theme-color" content="#f4f4f4" />
+			</Head>
+
+			<Script
+				src="https://www.googletagmanager.com/gtag/js?id=G-GPMR08P54M"
+				onLoad={() => {
+					window.dataLayer = window.dataLayer || [];
+					function gtag() {
+						dataLayer.push(arguments);
+					}
+					gtag('js', new Date());
+					gtag('config', 'G-GPMR08P54M');
+				}}
+			/>
 
 			<section id={styles.land}>
 				<div className={styles.image}>
@@ -40,18 +62,6 @@ export default function Home() {
 			<section className={styles.section}>
 				<h2 className={styles.titles}>Dicen de nosotros</h2>
 			</section>
-
-			<Script
-				src="https://www.googletagmanager.com/gtag/js?id=G-GPMR08P54M"
-				onLoad={() => {
-					window.dataLayer = window.dataLayer || [];
-					function gtag(){
-						dataLayer.push(arguments);
-					}
-					gtag('js', new Date());
-					gtag('config', 'G-GPMR08P54M');
-				}}
-			/>
 
 		</Layout >
 
